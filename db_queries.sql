67e20a02b253478499471a1cdf91b5510fd95b5d
blumend <dan.blumen1@gmail.com>
2019-01-21 00:10:26 +0200

    Delete db_queries.sql

--- db_queries.sql
+++
@@ -1,9 +0,0 @@
-Queries to minimize the DATABASE:
-
-DELETE FROM songs WHERE year=0;
-DELETE FROM songs WHERE title in (SELECT title FROM (SELECT title, LENGTH(title) AS a from songs) AS b WHERE b.a > 22);
-DELETE FROM songs WHERE artist_familiarity < 0.475;
-DELETE FROM artist_location WHERE location="";
-
-
-
