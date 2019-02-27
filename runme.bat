c7bdbe4b013ef6fe5ccbc5223d6f847bb6e25cc9
blumend <dan.blumen1@gmail.com>
2019-02-27 13:08:00 +0200

    Add files via upload

---
+++ runme.bat
@@ -0,0 +1,4 @@
+call C:\Users\User\Anaconda3\Scripts\activate.bat C:\Users\User\Anaconda3\envs\team16
+
+start chrome http://localhost:5200
+python "D:\DMWorkshop\fall2018-19\team16\Source\code\musical7boom.py"
